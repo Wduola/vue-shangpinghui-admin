7bf2681c0696204bed92cd74cb5a53da55388fb0
wangbaohong <q1329462887@gmail.com>
2020-05-23 00:42:28 +0800

    1. 平台属性添加与更新 2. 平台属性删除 3. SPU分页列表4. SPU更新界面初始数据请求

--- src/views/product/spu/list.vue
+++ src/views/product/spu/list.vue
@@ -6,57 +6,76 @@
     </el-card>
 
     <!-- spu列表 -->
-    <el-card class="box-card" border stripe style="margin-top:20px">
-      <div v-show="isShowList">
-        <el-table :data="attrs" border style="width: 100%; margin-top: 20px">
-          <el-table-column type="index" label="序号" width="80" align="center">
+    <el-card border stripe style="margin-top:20px">
+      <div v-show="!isShowSpuForm && !isShowSkuForm">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          style="margin-bottom: 20px"
+          >添加SPU</el-button
+        >
+        <el-table v-loading="loading" :data="spuList" border stripe>
+          <!-- 序号列 -->
+          <el-table-column label="序号" type="index" width="80" align="center">
           </el-table-column>
-          <el-table-column prop="attrName" label="SPU名称" width="400">
+          <el-table-column label="SPU名称" prop="spuName"> </el-table-column>
+          <el-table-column label="SPU描述" prop="description">
           </el-table-column>
-          <el-table-column prop="attrValueList" label="SPU描述">
-          </el-table-column>
-          <el-table-column prop="amount2" label="操作" width="150">
-            <!-- 如果列表列的内容是指定指定的结构: 使用作用域插槽 -->
-            <el-table-column prop="address" label="操作">
-              <template slot-scope="{ row }">
-                <el-button
-                  type="warning"
-                  icon="el-icon-edit"
-                  size="mini"
-                  @click="showUpdate(row)"
-                  >修改</el-button
-                >
-                <el-button
-                  type="danger"
-                  icon="el-icon-delete"
-                  size="mini"
-                  @click="deleteTrademark(row)"
-                  >删除</el-button
-                >
-              </template>
-            </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="{ row }">
+              <hint-button
+                title="添加SKU"
+                type="primary"
+                icon="el-icon-plus"
+                size="mini"
+                @click="showSkuAdd"
+              ></hint-button>
+              <hint-button
+                title="修改SPU"
+                type="primary"
+                icon="el-icon-edit"
+                size="mini"
+                @click="showUpdateSpu(row.id)"
+              ></hint-button>
+              <hint-button
+                title="查看所有SKU"
+                type="info"
+                icon="el-icon-info"
+                size="mini"
+              ></hint-button>
+              <hint-button
+                title="删除SPU"
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></hint-button>
+            </template>
           </el-table-column>
         </el-table>
+        <el-pagination
+          background
+          style="text-align: center; margin-top: 20px;"
+          :current-page="page"
+          :page-sizes="[3, 6, 9, 12]"
+          :page-size="limit"
+          layout="prev, pager, next, jumper, ->, sizes, total"
+          :total="total"
+          @current-change="getSpuList"
+          @size-change="handleSizeChange"
+        />
       </div>
-    </el-card>
 
-    <!-- 分页 -->
-    <el-pagination
-      background
-      style="text-align: center; margin-top: 20px;"
-      :total="total"
-      :current-page="page"
-      :page-sizes="[5, 10, 15, 20]"
-      :page-size="limit"
-      layout="prev, pager, next, jumper, ->,sizes, total"
-      @current-change="getTrademarks"
-      @size-change="handleSizeChange"
-    >
-    </el-pagination>
+      <SpuForm ref="spuForm" :visible.sync="isShowSpuForm"></SpuForm>
+
+      <SkuForm v-show="isShowSkuForm"></SkuForm>
+    </el-card>
   </div>
 </template>
 
 <script>
+import cloneDeep from "lodash/cloneDeep"; // 只引入要使用的工具函数
+import SpuForm from "../components/SpuForm";
+import SkuForm from "../components/SkuForm";
 export default {
   name: "SpuList",
 
@@ -65,34 +84,73 @@ export default {
       category1Id: "", // 一级分类ID
       category2Id: "", // 二级分类ID
       category3Id: "", // 三级分类ID
-      attrs: [], // 所有属性的列表
 
       isShowList: true, // 是否显示属性列表页面   true: 列表页面, false: 添加或更新页面
 
-      attr: {
-        // 要添加或者修改的平台属性对象
-        attrName: "", // 属性名
-        attrValueList: [], //属性值的列表
-        categoryId: "", // 3级的分类ID
-        categoryLevel: 3 // 只能是3级
-      },
+      spuList: [],
 
       trademarks: [], //当前页的列表数据
       total: 0, //总数量
       page: 1, //当前页码
       limit: 5, //每页数量
-      loading: false //图片加载
+      loading: false, //图片加载
+
+      isShowSpuForm: false,
+      isShowSkuForm: false
     };
   },
 
   mounted() {
-    this.category1Id = 2;
-    this.category2Id = 13;
     this.category3Id = 61;
-    this.getAttrs();
+    this.getSpuList();
   },
 
   methods: {
+    // 选择新的分类的监听回调
+    handleCategoryChange({ categoryId, level }) {
+      if (level === 1) {
+        this.category1Id = categoryId;
+        this.category2Id = "";
+        this.category3Id = "";
+        (this.spuList = []), (this.total = 0);
+      } else if (level === 2) {
+        this.category2Id = categoryId;
+        this.category3Id = "";
+        (this.spuList = []), (this.total = 0);
+      } else {
+        this.category3Id = categoryId;
+        // 请求获取分页列表数据
+        this.getSpuList();
+      }
+    },
+
+    // 显示SPU的修改界面
+    showUpdateSpu(id) {
+      // 显示SpuForm修改界面
+      this.isShowSpuForm = true;
+
+      // 通知SpuForm根据传入的ID请求获取初始显示需要的数据
+      // 使用的是v-show来隐藏的, 隐藏时组件对象还在存在
+      this.$refs.spuForm.initLoadUpdateData(id);
+    },
+
+    // 显示SKU添加的表单界面
+    showSkuAdd() {
+      this.isShowSkuForm = true;
+    },
+
+    // 获取指定页码的列表数据 (spuList, total)
+    async getSpuList(page = 1) {
+      this.page = page;
+      const { limit, category3Id } = this;
+      const result = await this.$API.spu.getList(page, limit, category3Id);
+      if (result.code === 200) {
+        const { records, total } = result.data;
+        this.spuList = records;
+        this.total = total;
+      }
+    },
+
     // 3个级别分类发生改变时的监听回调
     handleCategoryChange({ categoryId, level }) {
       // console.log('handleCategoryChange', categoryId, level)
@@ -151,7 +209,23 @@ export default {
       this.limit = pageSize;
       // 重新请求获取第一页列表显示
       this.getTrademarks();
+    },
+
+    // 显示修改属性的界面
+    showUpdate(attr) {
+      // 保存要修改的属性对象
+      // this.attr = attr // 列表与修改界面引用了同一个属性对象  ==> 修改属性名不能取消
+      // this.attr = {...attr} // 对attr进行了一个浅拷贝(克隆)  ==> 修改属性值名称不能取消
+      this.attr = cloneDeep(attr); // 对attr进行了一个深拷贝(克隆) ==> OK
+
+      // 显示更新的界面(attr中有数据)
+      this.isShowList = false;
     }
+  },
+
+  components: {
+    SpuForm,
+    SkuForm
   }
 };
 </script>
